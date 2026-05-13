f25d241c08f26a1673e166f61ecee59190263aae
Пасовистий Юліан <julianpasovystyy@gmail.com>
2026-05-14 00:26:26 +0300

    Добавив звуки в гловне меню та паузу, звук стрибка та бігу

--- scripts/main_sprite.gd
+++ scripts/main_sprite.gd
@@ -1,5 +1,7 @@
 extends CharacterBody2D
 
+const SOUND_WALK:= preload("res://Sounds/wova-zvuki-shagi-beg-po-betonnomu-polu (mp3cut.net) (2).mp3")
+const SOUND_JUMP:= preload("res://Sounds/c3a799694397ac6.mp3")
 
 const SPEED = 350.0
 const JUMP_VELOCITY = -900.0
@@ -19,12 +21,21 @@ func _physics_process(delta: float) -> void:
 
 	# Handle jump.
 	if Input.is_action_just_pressed("jump") and is_on_floor():
+		$movesound.stream = SOUND_JUMP
+		$movesound.pitch_scale = 1.0
+		$movesound.play()
+		$movesound/Timer.start()
 		velocity.y = JUMP_VELOCITY
 
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
 	var direction := Input.get_axis("left", "right")
 	if direction:
+		if $movesound/Timer.time_left==0 and is_on_floor():
+			$movesound.stream = SOUND_WALK
+			$movesound.pitch_scale = 1.0 + randf_range(-0.1 , 0.1)
+			$movesound.play()
+			$movesound/Timer.start()
 		velocity.x = direction * SPEED
 	else:
 		velocity.x = move_toward(velocity.x, 0, 17)
